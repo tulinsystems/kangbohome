2ce34d9212a0fd3dd85fe950a1e76f7f682a35a5
Daniel Toussaint <daniel@dmhome.net>
2015-04-20 12:41:42 +0800

    * Added localhost:3000 as a default. * Made Linux Deployer startup at boot and execute our startup script. * Added fullscreen * TODO : navigation bar and home launcher (later, when development is done)

--- app/src/main/java/com/tulinsystems/kangbohome/KangboActivity.java
+++ app/src/main/java/com/tulinsystems/kangbohome/KangboActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 
 import org.xwalk.core.XWalkView;
 import org.xwalk.core.XWalkPreferences;
@@ -18,10 +20,18 @@ public class KangboActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        /* Go full screen */
+        requestWindowFeature(getWindow().FEATURE_ACTION_BAR_OVERLAY);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_kangbo);
 
+        /* Browser view - Cross Walk*/
         mView = (XWalkView) findViewById(R.id.xView);
-        mView.load("http://www.tulinsystems.com", null);
+        mView.load("http://localhost:3000", null);
 
         //debugging
         //XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
